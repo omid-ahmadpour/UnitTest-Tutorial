a118872371cde44be7efd3780d9482936b932c50
omid ahmadpour <ahmadpooromid@gmail.com>
2023-11-11 22:53:35 +0100

    add more unit test for EmailValidator. episode part 5.

--- test/UnitTestTutorial.Persistence.Test/EmailValidatorTests.cs
+++ test/UnitTestTutorial.Persistence.Test/EmailValidatorTests.cs
@@ -18,5 +18,47 @@ namespace UnitTestTutorial.Persistence.Tests.CleanTemplate.Persistence.Tests
             // Assert
             Assert.IsFalse(result);
         }
+
+        [Test]
+        public void IsValidEmail_ValidEmail_ReturnsTrue()
+        {
+            // Arrange
+            var validator = new EmailValidator();
+            string email = "user@gmail.com";
+
+            // Act
+            var result = validator.IsValidEmail(email);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void IsValidEmail_WhiteSpaceEmail_ReturnsFalse()
+        {
+            // Arrange
+            var validator = new EmailValidator();
+            string email = " ";
+
+            // Act
+            var result = validator.IsValidEmail(email);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void IsValidEmail_EmptyEmail_ReturnsFalse()
+        {
+            // Arrange
+            var validator = new EmailValidator();
+            string email = string.Empty;
+
+            // Act
+            var result = validator.IsValidEmail(email);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
     }
 }
