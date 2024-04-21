8237871139c1d7d7c817b7acdffbf5cb9866af0c
omid ahmadpour <ahmadpooromid@gmail.com>
2024-04-21 11:23:15 +0200

    Spy example in real project.

---
+++ TestDoubles/UnitTestTutorial.TestDoubles.Tests/LoggerTests.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+
+namespace UnitTestTutorial.TestDoubles.Tests
+{
+    public class LoggerTests
+    {
+        // Spy Example
+        [Test]
+        public void LogError_CallsLogMethod_WithCorrectErrorMessage()
+        {
+            // Arrange
+            var loggerSpy = new Mock<ILogger>();
+            var logger = new Logger(loggerSpy.Object);
+
+            // Act
+            logger.LogError("Something went wrong");
+
+            // Assert
+            loggerSpy.Verify(log => log.Log("Error: Something went wrong"), Times.Once);
+        }
+    }
+}
---
+++ TestDoubles/UnitTestTutorial.TestDoubles/ILogger.cs
@@ -0,0 +1,22 @@
+﻿namespace UnitTestTutorial.TestDoubles
+{
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+
+    public class Logger
+    {
+        private readonly ILogger _logger;
+
+        public Logger(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void LogError(string errorMessage)
+        {
+            _logger.Log($"Error: {errorMessage}");
+        }
+    }
+}
