5ffcaa952d7513b1b434c6378ebadb96bdc33c4e
omid ahmadpour <ahmadpooromid@gmail.com>
2024-04-21 11:11:27 +0200

    Stub Example in a real project.

---
+++ TestDoubles/UnitTestTutorial.TestDoubles.Tests/TaxCalculatorTests.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+
+namespace UnitTestTutorial.TestDoubles.Tests
+{
+    public class TaxCalculatorTests
+    {
+        [Test]
+        public void GetTotalSalaryAfterTax_ReturnsCorrectAmount_AfterTaxCalculation()
+        {
+            // Arrange
+            var taxCalculatorStub = new Mock<ITaxCalculator>();
+
+            // Stub for tax calculation
+            taxCalculatorStub.Setup(taxCalc => taxCalc.CalculateTax(1000)).Returns(100);
+            var calculator = new Calculator(taxCalculatorStub.Object);
+
+            // Act
+            var result = calculator.GetTotalSalaryAfterTax(1000);
+
+            // Assert
+            Assert.AreEqual(900, result);
+        }
+    }
+}
---
+++ TestDoubles/UnitTestTutorial.TestDoubles/ITaxCalculator.cs
@@ -0,0 +1,23 @@
+﻿namespace UnitTestTutorial.TestDoubles
+{
+    public interface ITaxCalculator
+    {
+        decimal CalculateTax(decimal income);
+    }
+
+    public class Calculator
+    {
+        private readonly ITaxCalculator _taxCalculator;
+
+        public Calculator(ITaxCalculator taxCalculator)
+        {
+            _taxCalculator = taxCalculator;
+        }
+
+        public decimal GetTotalSalaryAfterTax(decimal income)
+        {
+            var tax = _taxCalculator.CalculateTax(income);
+            return income - tax;
+        }
+    }
+}
