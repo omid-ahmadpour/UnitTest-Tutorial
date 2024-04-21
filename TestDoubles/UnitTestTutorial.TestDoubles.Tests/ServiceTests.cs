f3053a9e438f35ef18ddcdf5a46b7c5d31b7d329
omid ahmadpour <ahmadpooromid@gmail.com>
2024-04-21 11:39:46 +0200

    Fake Example in a real project.

---
+++ TestDoubles/UnitTestTutorial.TestDoubles.Tests/ServiceTests.cs
@@ -0,0 +1,21 @@
+﻿namespace UnitTestTutorial.TestDoubles.Tests
+{
+    public class ServiceTests
+    {
+        // Fake Example
+        [Test]
+        public void Save_SavesDataToFakeDatabase()
+        {
+            // Arrange
+            var fakeDatabase = new FakeDatabase();
+            var data = "Sample data";
+            var service = new Service(fakeDatabase);
+
+            // Act
+            service.SaveData(data);
+
+            // Assert
+            Assert.Contains(data, fakeDatabase.GetAllData());
+        }
+    }
+}
---
+++ TestDoubles/UnitTestTutorial.TestDoubles/IService.cs
@@ -0,0 +1,45 @@
+﻿namespace UnitTestTutorial.TestDoubles
+{
+    public interface IDatabase
+    {
+        void Save(string data);
+    }
+
+    public class Database : IDatabase
+    {
+        public void Save(string data)
+        {
+            // Actual implementation to save data to the database
+        }
+    }
+
+    public class FakeDatabase : IDatabase
+    {
+        private readonly List<string> _data = new List<string>();
+
+        public void Save(string data)
+        {
+            _data.Add(data);
+        }
+
+        public List<string> GetAllData()
+        {
+            return _data;
+        }
+    }
+
+    public class Service
+    {
+        private readonly IDatabase _database;
+
+        public Service(IDatabase database)
+        {
+            _database = database;
+        }
+
+        public void SaveData(string data)
+        {
+            _database.Save(data);
+        }
+    }
+}
