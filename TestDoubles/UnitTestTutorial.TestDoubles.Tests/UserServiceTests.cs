f4bffb2fd1fca5550588044b20c579cc9ec784d9
omid ahmadpour <ahmadpooromid@gmail.com>
2024-04-21 11:13:34 +0200

    Mock Example in a real project.

---
+++ TestDoubles/UnitTestTutorial.TestDoubles.Tests/UserServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+
+namespace UnitTestTutorial.TestDoubles.Tests
+{
+    using NUnit.Framework;
+
+    // Mock Example
+    [TestFixture]
+    public class UserServiceTests
+    {
+        [Test]
+        public void GetUserNameById_ReturnsUserName_WhenUserExists()
+        {
+            // Arrange
+            var userRepositoryMock = new Mock<IUserRepository>();
+            userRepositoryMock.Setup(repo => repo.GetUserById(1)).Returns(new User { Name = "John" });
+            var userService = new UserService(userRepositoryMock.Object);
+
+            // Act
+            var result = userService.GetUserNameById(1);
+
+            // Assert
+            Assert.AreEqual("John", result);
+        }
+    }
+}
---
+++ TestDoubles/UnitTestTutorial.TestDoubles/IUserRepository.cs
@@ -0,0 +1,28 @@
+﻿namespace UnitTestTutorial.TestDoubles
+{
+    public interface IUserRepository
+    {
+        User GetUserById(int userId);
+    }
+
+    public class UserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public string GetUserNameById(int userId)
+        {
+            var user = _userRepository.GetUserById(userId);
+            return user != null ? user.Name : null;
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+    }
+}
